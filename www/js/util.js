3756426f00bd4689588c7d27e7047d8454fa62de
langel <knoxburry@gmail.com>
2024-10-14 18:06:29 -0400

    util functions should be const (not let)

--- www/js/util.js
+++ www/js/util.js
@@ -4,15 +4,18 @@ const blank = "&#x2800;";
 const br = "<br>";
 
 // value convertors
-let char = (ord) => String.fromCharCode(ord);
-let ord = (char) => char.charCodeAt(0);
-let tohex = (x) => x.toString(16).padStart(2, '0'); 
+const char = (ord) => String.fromCharCode(ord);
+const ord = (char) => char.charCodeAt(0);
+const tohex = (x) => x.toString(16).padStart(2, '0'); 
 
 // DOM
-let element_new = (t) => document.createElement(t);
-let frame_next = () => { return new Promise(resolve => requestAnimationFrame(resolve)); }
-let tobottom = () => window.scrollTo(0, document.body.scrollHeight);
+const element_new = (t) => document.createElement(t);
+const frame_next = () => { return new Promise(resolve => requestAnimationFrame(resolve)); }
+const tobottom = () => window.scrollTo(0, document.body.scrollHeight);
 
 // local storage
-let localget = (key) => localStorage.getItem(key);
-let localset = (key, val) => localStorage.setItem(key, val);
+const localget = (key) => localStorage.getItem(key);
+const localset = (key, val) => localStorage.setItem(key, val);
+
+// arrays
+
