2205424146cbd4dcffd3fd7f9855b05fcc3e1444
langel <knoxburry@gmail.com>
2024-10-18 18:31:42 -0400

    notes on bpp data structures

--- www/js/processors/chr.js
+++ www/js/processors/chr.js
@@ -3,9 +3,10 @@ const process_chr = (data) => {
 	logout('processing chr data ::');
 /* TO DO
 	-- schema choices:
-		2bpp 8x8   (nes)
-		4bpp 8x8   (pce bg tiles)
-		4bpp 16x16 (pce sprites)
+		2bpp 8x8   planar (nes)
+		4bpp 8x8   planar (snes(all)/pce bg tiles)
+		4bpp 8x8   linear (megadrive)
+		4bpp 16x16 planar (pce sprites)
 	-- draw to a canvas
 	-- editing would be out of scope for this file
 		should parallel update data and canvas
