0f22f7ac33f82cd86400e1dbcd6cfd5394380d67
langel <knoxburry@gmail.com>
2024-11-04 18:04:21 -0500

    audio system notes

---
+++ www/js/audio.js
@@ -0,0 +1,63 @@
+/*
+
+
+skrontch audio system notes
+
+unique tracker 
+supports sfx and instrument macros that overwrite bg music
+
+playback in emulator as rom
+
+
+
+
+
+
+
+some esoteric desired tunings:
+
+Gamelan:
+
+Ranat ek bar
+
+gong    Morton's  Morton's   actual
+number  readings  intervals  intervals (cents)
+
+   21    1220        
+   20    1098        178        182.4
+   19     998        176        165.3
+   18     896        187        186.7
+   17     818        160        157.7
+   16     740        167        173.5
+   15     672        175        166.9
+   14     604        177        184.7
+
+
+Khawng wong lek
+
+gong    Morton's  Morton's   actual
+number  readings  intervals  intervals (cents)
+
+   8       1224
+   7       1106       178      175.5
+   6        994       176      184.8
+   5        894       187      183.6
+   4        812       160      166.6
+   3        738       167      165.4
+   2        664       175      182.9
+   1        602       177      169.7
+
+
+Japense:
+
+5 tet/edo?
+
+
+Chinese:
+
+archaic divide/multiply system with non-aligned octaves
+
+traditional 12 tone alternative system
+
+
+*/
